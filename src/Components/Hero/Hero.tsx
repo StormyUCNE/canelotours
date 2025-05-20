7f45d9641859382aabb9fff00673aabafaa8201f
StormySilverio05 <stormysilverionunez@gmail.com>
2025-05-20 01:34:40 -0400

    fff

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,42 +1,27 @@
-import PayMethods from "../../assets/Imgs/Hero/Paymentmethods.webp";
 import HeroDesktop from "../../assets/Imgs/Hero/HeroImg.webp"
 import HeroMobile from "../../assets/Imgs/Hero/HeroImgMobile.webp"
-import LogoMobile from "../../assets/Imgs/Hero/HeroLogoMobile.webp";
-import Formimg from "../../assets/Imgs/Hero/FormImg.png";
-import { useLanguage } from "../Language/LanguageContext";
+
 function Hero() {
-const {texts} = useLanguage();
   return (
     <div className="HeroContainer">
       <picture className="HeroBackground">
-          <source
-            srcSet={HeroDesktop}
-            media="(min-width: 768px)"
-            type="image/webp"
-          />
-          <img
-            src={HeroMobile}
-            alt="Descripción del hero"
-            fetchPriority="high"
-          />
-        </picture>
-      <div className="Content">
-        <img src={LogoMobile} alt="Canelo Tours Logo" width={220} height={220}  />
-        <h1>
-          {texts.HeroSection.HeroTitle}
-        </h1>
-        <p>
-          {texts.HeroSection.Description}
-        </p>
-        <button>
-          <img src={Formimg} alt="Calendar Icon" width={25} height={25} />
-          <p>{texts.HeroSection.ReservationButton}</p>
-        </button>
-        <p>
-          {texts.HeroSection.Price}
-        </p>
-        <img className="paymethods" src={PayMethods} alt="Payment Methods" width={800} height={94}/>
-      </div>
+  <source
+    srcSet={HeroDesktop}
+    media="(min-width: 768px)"
+    type="image/webp"
+    width={1920}  // Añade dimensiones reales
+    height={1080}
+  />
+  <img
+    src={HeroMobile}
+    alt="Descripción del hero"
+    fetchPriority="high"
+    width={768}   // Añade dimensiones reales
+    height={1024}
+    loading="eager" // Ya que es el hero
+  />
+</picture>
+      
     </div>
   )
 }
