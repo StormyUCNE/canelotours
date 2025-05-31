1adea5d231ddf3d44b089333916ff05a89bfa80f
StormySilverio05 <stormysilverionunez@gmail.com>
2025-05-31 19:50:33 -0400

    vvvv

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -12,26 +12,22 @@ function Hero() {
           srcSet={HeroDesktop}
           media="(min-width: 768px)"
           type="image/webp"
-          width={2000} 
-          height={1333}
         />
         <img
           src={HeroMobile}
           alt="Descripción del hero"
           fetchPriority="high"
           loading="eager"
-          width={600}   
-          height={400}
           decoding="async"
         />
       </picture>
       <div className="HeroContent">
-        <img className="Logo" src={Logo} alt="Hero Logo" fetchPriority="high" decoding="async" width={500} height={500} />
+        <img className="Logo" src={Logo} alt="Hero Logo" fetchPriority="high" width={500} height={500} />
         <p className="Fisttitle">{texts.HeroSection.HeroTitle}</p>
         <p className="Description">{texts.HeroSection.Description}</p>
         <a className="BookingButton" href="#">{texts.HeroSection.ReservationButton}</a>
         <p className="Price">{texts.HeroSection.Price}</p>
-        <img className="Payments" src={Paymentmethods} decoding="async" fetchPriority="high" alt="Payment methods" width={1412} height={166}  />
+        <img className="Payments" src={Paymentmethods} fetchPriority="high" alt="Payment methods" width={1412} height={166}  />
       </div>
     </div>
   )
