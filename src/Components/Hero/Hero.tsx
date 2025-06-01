6b2fb213f2165d47abb799cca2d044d55f24ed57
StormySilverio05 <stormysilverionunez@gmail.com>
2025-05-31 21:21:36 -0400

    dd

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,12 +1,37 @@
-
-
+import HeroDesktop from "../../assets/Imgs/Hero/HeroImg.webp"
+import HeroMobile from "../../assets/Imgs/Hero/HeroImgMobile.webp"
+import { useLanguage } from "../Language/LanguageContext"
+import Logo from "../../assets/Imgs/Hero/HeroLogo.webp"
+import Paymentmethods from "../../assets/Imgs/Hero/Paymentmethods.webp"
 function Hero() {
-  
+  const {texts} = useLanguage();
   return (
     <div className="HeroContainer">
-      
+      <picture className="HeroBackground">
+        <source
+          srcSet={HeroDesktop}
+          media="(min-width: 768px)"
+          type="image/webp"
+          width={2000} 
+          height={1333}
+        />
+        <img
+          src={HeroMobile}
+          alt="Hero Background"
+          fetchPriority="high"
+          loading="eager"
+          width={800}   
+          height={533}
+          decoding="async"
+        />
+      </picture>
       <div className="HeroContent">
-        
+        <img className="Logo" src={Logo} alt="Hero Logo" fetchPriority="high" />
+        <p className="Fisttitle">{texts.HeroSection.HeroTitle}</p>
+        <p className="Description">{texts.HeroSection.Description}</p>
+        <a className="BookingButton" href="#">{texts.HeroSection.ReservationButton}</a>
+        <p className="Price">{texts.HeroSection.Price}</p>
+        <img className="Payments" src={Paymentmethods} decoding="async" fetchPriority="high" alt="Payment methods" />
       </div>
     </div>
   )
